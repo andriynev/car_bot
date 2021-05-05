8d3916da91416e5bc806589e9e25da7383e50fba
andriynev <nevmerzhytskyiandriy@gmail.com>
2021-05-06 01:22:44 +0300

    create spring boot app

---
+++ src/main/java/com/andriynev/driver_helper_bot/DriverHelperBotApplication.java
@@ -0,0 +1,13 @@
+package com.andriynev.driver_helper_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverHelperBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverHelperBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andriynev/driver_helper_bot/DriverHelperBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriynev.driver_helper_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverHelperBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
